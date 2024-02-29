1a4af51007862051c0abb5f9770afabdebcaf431
teja.kambam <teja.kambam@trianz.com>
2024-02-29 14:39:33 +0530

    modified dashboard css

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,7 +6,7 @@ import ArticleIcon from "@mui/icons-material/Article";
 import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css"; // Import the CSS file
 import { CustomizedMenus } from "./MenuButton";
-import { configurations, organizations, resources, servers, users } from "../../assets/constants";
+import { configurations, opnId, organizations, resources, servers, users } from "../../assets/constants";
 
 
 const Header = () => {
@@ -21,12 +21,11 @@ const Header = () => {
           className={styles["nav-link"]}
           style={{
             background: "rgb(0 100 155)",
-            width: "80px",
             fontSize: "12px",
           }}
         >
 
-          <Button style={{ background: 'rgb(0 100 155)', width: '128px', fontSize: '12px',color:'#ffffff' }} onClick={handleDashboardBtn}>
+          <Button style={{ background: 'rgb(0 100 155)', fontSize: '12px',color:'#ffffff' }} onClick={handleDashboardBtn}>
             <AccessTime style={{ fontSize: "18px", padding: "4px" }} />
             DASHBOARD
           </Button>
@@ -47,15 +46,8 @@ const Header = () => {
         <Typography className={styles["nav-link"]}>
           <CustomizedMenus menuConfig={configurations} />
         </Typography>
-        <Typography
-          className={styles["nav-link"]}
-          style={{
-            background: "rgb(0 100 155)",
-            fontSize: "12px",
-          }}
-        >
-          <ArticleIcon style={{ fontSize: "18px", padding: "4px" }} />
-          OPENID CONNECT
+        <Typography className={styles["nav-link"]}>
+          <CustomizedMenus menuConfig={opnId} />
         </Typography>
       </Grid>
     </Grid>
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -16,33 +16,36 @@ export const DashboardPage: React.FC = () => {
     {typeName:"External Customer",value:0}
   ]
   return (
-    <Grid container md={10} margin={'12px auto'}>
-        <Grid md={12} display={'flex'} justifyContent={'space-between'}>
+    <Grid container margin={'12px auto'}>
+      <Grid container display={'flex'} justifyContent={'space-between'}>
         <Typography color='#00649b' fontWeight={600}>Dashboard</Typography>
-        <Typography display={'flex'} alignItems={'center'} columnGap={'4px'}><AutorenewTwoTone/> Refresh</Typography>
+        <Typography display={'flex'} ><AutorenewTwoTone/> Refresh</Typography>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard background='#8674a7' title="Active Users" count={3} IconComponent={Groups} />
         </Grid>
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} md={3}>
-        <InfoCard background='#8674a7' title="Active Users" count={3} IconComponent={Groups} />
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard background='#ffb240' title="Password Expired Users" count={0} IconComponent={Groups} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard background='#e55a58' title="Inactive Users" count={0} IconComponent={Groups} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <InfoCard background='#ffb240' title="Locked Users" count={0} IconComponent={Groups} />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <InfoCard background='#ffb240' title="Password Expired Users" count={0} IconComponent={Groups} />
+      <Grid container spacing={2} sx={{ my: 2}} >
+        <Grid item xs={12} sm={6} md={4}>
+          <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <InfoCard background='#e55a58' title="Inactive Users" count={0} IconComponent={Groups} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <InfoCard background='#ffb240' title="Locked Users" count={0} IconComponent={Groups} />
-      </Grid>
-    </Grid>
-    <Grid display={'flex'} columnGap={"12px"} margin={'16px auto'}>
-      <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
-      <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
-      <ListBox resources={userTypes} title='User Types' subtitle='No. of Users' IconComponent={People}  />
-    </Grid>
-    <Grid>
-
-    </Grid>
     </Grid>
   );
 };
